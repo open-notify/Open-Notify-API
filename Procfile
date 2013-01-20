6982972733914e626b0921b0a49d29b08da5d294
Nathan Bergey <nathan.bergey@gmail.com>
2013-01-19 18:18:12 -0800

    froze pip and added a procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn app:app -b "0.0.0.0:$PORT" -w 3
--- update.py
+++ update.py
@@ -1,12 +1,54 @@
 import redis
 import json 
+import urllib2
+import datetime
+import time
 
 r = redis.StrictRedis(host="127.0.0.1", port=6379, db=0)
 
 
-tle = json.dumps(["ISS (ZARYA)",
-    "1 25544U 98067A   13019.24297745  .00019637  00000-0  32663-3 0   523",
-    "2 25544  51.6466 128.2329 0013922 174.5549 270.6743 15.51955285811576"])
+# NASA's station FDO updates this page with very precise data. Only using a 
+# small bit of it for now.
+url = "http://spaceflight.nasa.gov/realdata/sightings/SSapplications/Post/JavaSSOP/orbit/ISS/SVPOST.html"
 
+# Open a http request
+req = urllib2.Request(url)
+response = urllib2.urlopen(req)
+data = response.read()
 
-r.set("iss_tle", tle)
+# parse the HTML
+data = data.split("<PRE>")[1]
+data = data.split("</PRE>")[0]
+data = data.split("Vector Time (GMT): ")[1:]
+
+for group in data:
+    # Time the vector is valid for
+    datestr = group[0:17]
+
+    # parse date string
+    tm = time.strptime(datestr, "%Y/%j/%H:%M:%S")
+
+    # change into more useful datetime object
+    dt = datetime.datetime(tm[0], tm[1], tm[2], tm[3], tm[4], tm[5])
+
+    # More parsing
+    tle = group.split("TWO LINE MEAN ELEMENT SET")[1]
+    tle = tle[8:160]
+    lines = tle.split('\n')[0:3]
+
+    # Most recent TLE
+    now = datetime.datetime.utcnow()
+    if (dt - now).days >= 0:
+        # Debug Printing
+        """
+        print dt
+        for line in lines:
+            print line.strip()
+        print "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+        """
+
+        tle = json.dumps( [lines[0].strip()
+                         , lines[1].strip()
+                         , lines[2].strip()])
+
+        r.set("iss_tle", tle)
