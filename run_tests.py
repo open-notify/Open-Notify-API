1b09b9862f4ddf28afbccfe83059130f3d86eb41
Nathan Bergey <nathan.bergey@gmail.com>
2013-11-20 15:43:09 -0800

    tests with flask-WebTest

---
+++ run_tests.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import unittest
+from testsuite.web import *
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ testsuite/web.py
@@ -0,0 +1,17 @@
+from unittest import main, TestCase
+from flask.ext.webtest import TestApp
+from app import app
+
+class FrontPageTest(TestCase):
+    def setUp(self):
+        self.app = app
+        self.w = TestApp(self.app)
+
+    def test(self):
+        r = self.w.get('/')
+
+        # Assert there was no messages flashed
+        self.assertFalse(r.flashes)
+
+        # Assert that we got an index template
+        self.assertEqual(r.template, 'index.html')
