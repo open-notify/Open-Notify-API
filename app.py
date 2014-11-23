7e2740dfafdb8b2a2477e33d7e77f3dab223e963
Nathan Bergey <nathan.bergey@gmail.com>
2014-11-23 12:17:56 -0800

    Soyuz TMA-15M launch

--- app.py
+++ app.py
@@ -126,6 +126,9 @@ def astros():
         {'name': "Alexander Samokutyaev",   'craft': "ISS"},
         {'name': "Elena Serova",            'craft': "ISS"},
         {'name': "Barry Wilmore",           'craft': "ISS"},
+        {'name': "Anton Schkaplerov",       'craft': "Soyuz TMA-15M"},
+        {'name': "Samantha Cristoforetti",  'craft': "Soyuz TMA-15M"},
+        {'name': "Terry Virts",             'craft': "Soyuz TMA-15M"},
     ]
     return {'message': "success", 'number': len(Astros), 'people': Astros}, 200
 
